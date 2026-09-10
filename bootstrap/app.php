c6263f1e4a09e01fbc0ea2ee394f0acef12b53c2
repogit <alanyolo237@gmail.com>
2026-09-09 19:13:49 -0600

    Configurar proxy HTTPS para Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,13 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
+        // Render funciona detrás de un proxy HTTPS
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
